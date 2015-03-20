415580b9f1331129e3f4494defb7555e845dfa77
mqn2at <minhqn127@gmail.com>
2015-03-20 11:15:29 -0400

    Added entity and player+collide components

---
+++ LittleRedRobinHood/LittleRedRobinHood/Component/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LittleRedRobinHood.Component
+{
+    class Player
+    {
+        public readonly int entityID;
+        public readonly int MAX_ARROWS = 3;
+        public readonly int MAX_SHACKLES = 3;
+        public int health = 3;
+        public int arrows;
+        public int shackles;
+        public bool grounded;
+        public bool running;
+        public bool jumping;
+        public Player(int id)
+        {
+            this.entityID = id;
+            this.arrows = MAX_ARROWS;
+            this.shackles = MAX_SHACKLES;
+            this.grounded = true;
+            this.running = false;
+            this.jumping = false;
+        }
+    }
+}
