6449c581138bfef3ba967b83d48187ce4cb058f3
mqn2at <minhqn127@gmail.com>
2015-03-23 19:44:38 -0400

    Added Systems

---
+++ LittleRedRobinHood/LittleRedRobinHood/System/CollisionSystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LittleRedRobinHood.Component;
+using LittleRedRobinHood.Entity;
+
+namespace LittleRedRobinHood.System
+{
+    class CollisionSystem
+    {
+        public void collide(List<Entity.Entity> entities, List<Collide> collideables)
+        {
+            for (int i = 0; i < collideables.Count - 1; i++)
+            {
+
+            }
+                
+        }
+    }
+}
---
+++ LittleRedRobinHood/LittleRedRobinHood/System/ControlSystem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+
+namespace LittleRedRobinHood.System
+{
+    class ControlSystem
+    {
+        public KeyboardState kb;
+        public KeyboardState kbo;
+
+        public ControlSystem()
+        {
+            this.kb = Keyboard.GetState();
+            this.kbo = Keyboard.GetState();
+        }
+
+        public void Update()
+        {
+            kbo = kb;
+            kb = Keyboard.GetState();
+        }
+
+        public bool isPressed(Keys key)
+        {
+            //Console.WriteLine (button);
+            return kb.IsKeyDown(key);
+        }
+
+        public bool onPress(Keys key)
+        {
+            return kb.IsKeyDown(key) && kbo.IsKeyUp(key);
+        }
+
+        public bool onRelease(Keys key)
+        {
+            //Console.WriteLine (button);
+            return kb.IsKeyUp(key) && kbo.IsKeyDown(key);
+        }
+
+        public bool isHeld(Keys key)
+        {
+            //Console.WriteLine (button);
+            return kb.IsKeyDown(key) && kbo.IsKeyDown(key);
+        }
+    }
+}
