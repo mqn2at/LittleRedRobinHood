20e3f16e2492495cc8f240f39e42516172a3f5ae
sjboris <stephenboris@gmail.com>
2015-03-23 19:40:58 -0400

    included planned E and C of ECS

---
+++ LittleRedRobinHood/LittleRedRobinHood/Component/Homing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LittleRedRobinHood.Component
+{
+    class Homing
+    {
+        public readonly bool isFlying;
+        public readonly string myLogic;
+
+        public Homing(bool fly, string commands)
+        {
+            this.isFlying = fly;
+            this.myLogic = commands;
+        }
+    }
+}
---
+++ LittleRedRobinHood/LittleRedRobinHood/Component/Text.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace LittleRedRobinHood.Component
+{
+    class Text
+    {
+        public readonly Vector2 triggerPosition;
+        public readonly Vector2 textPosition;
+        public readonly string text;
+        public bool visible;
+
+        public Text(Vector2 posTrigger, Vector2 posText, string text)
+        {
+            this.triggerPosition = posTrigger;
+            this.textPosition = posText;
+            this.text = text;
+            visible = false;
+        }
+    }
+}
