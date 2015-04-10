0f786185af975cc536fd58dae8837acd9373443d
sjboris <stephenboris@gmail.com>
2015-04-09 21:27:49 -0400

    fixed texts in places cause reasons

--- LittleRedRobinHood/LittleRedRobinHood/Component/Text.cs
+++ LittleRedRobinHood/LittleRedRobinHood/Component/Text.cs
@@ -10,36 +10,32 @@ namespace LittleRedRobinHood.Component
     class Text
     {
         public readonly SpriteFont font;
-        public readonly Vector2 triggerPosition;
         public readonly Vector2 textPosition;
         public readonly string text;
         public bool visible;
         public Single scale;
 
-        public Text(SpriteFont font, Vector2 posTrigger, Vector2 posText, string text)
+        public Text(SpriteFont font, Vector2 posText, string text)
         {
             this.font = font;
-            this.triggerPosition = posTrigger;
             this.textPosition = posText;
             this.text = text;
             visible = false;
             this.scale = 1;
         }
 
-        public Text(SpriteFont font, Vector2 posTrigger, Vector2 posText, string text, bool visible)
+        public Text(SpriteFont font, Vector2 posText, string text, bool visible)
         {
             this.font = font;
-            this.triggerPosition = posTrigger;
             this.textPosition = posText;
             this.text = text;
             this.visible = visible;
             this.scale = 1;
         }
 
-        public Text(SpriteFont font, Vector2 posTrigger, Vector2 posText, string text, bool visible, Single scale)
+        public Text(SpriteFont font, Vector2 posText, string text, bool visible, Single scale)
         {
             this.font = font;
-            this.triggerPosition = posTrigger;
             this.textPosition = posText;
             this.text = text;
             this.visible = visible;
