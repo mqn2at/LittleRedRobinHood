8dd8ee56cf2d15e5cfe8e25c588539b2b79bca00
Peter Shao <pds9vc@virginia.edu>
2015-04-09 22:27:45 -0400

    change to brown

--- LittleRedRobinHood/LittleRedRobinHood/LittleRedRobinHoodGame.cs
+++ LittleRedRobinHood/LittleRedRobinHood/LittleRedRobinHoodGame.cs
@@ -403,7 +403,7 @@ namespace LittleRedRobinHood
                 (float)Math.Atan2(edge.Y, edge.X);
             Texture2D t = new Texture2D(GraphicsDevice, 1, 1);
             t.SetData<Color>(
-                new Color[] { Color.Black });
+                new Color[] { Color.SaddleBrown});
 
             sb.Draw(t,
                 new Rectangle(// rectangle defines shape of line and position of start of line
@@ -412,7 +412,7 @@ namespace LittleRedRobinHood
                     (int)edge.Length(), //sb will strech the texture to fill this rectangle
                     5), //width of line, change this to make thicker line
                 null,
-                Color.Black, //colour of line
+                Color.SaddleBrown, //colour of line
                 angle,     //angle of line (calulated above)
                 new Vector2(0, 0), // point in line about which to rotate
                 SpriteEffects.None,
