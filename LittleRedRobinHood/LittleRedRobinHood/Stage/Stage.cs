d4a672f3115858c266c4ef5c6e43e28406bc9c74
sjboris <stephenboris@gmail.com>
2015-04-23 21:42:39 -0400

    fixed acorns kind of

--- LittleRedRobinHood/LittleRedRobinHood/Stage/Stage.cs
+++ LittleRedRobinHood/LittleRedRobinHood/Stage/Stage.cs
@@ -365,7 +365,7 @@ namespace LittleRedRobinHood
                 waypoints.Add(new Vector2(x, y));
                 waypoints.Add(new Vector2(x, y + 500));
                 cm.addPatrol(tempID, waypoints, 4, false);
-                cm.addProjectile(tempID, true, Math.PI / 2, 1);
+                cm.addProjectile(tempID, true, Math.PI / 2, 1, o.X, o.Y);
                 cm.addSprite(tempID, o.Width, o.Height, content.Load<Texture2D>("pinecone.png"), pineconeSpriteBox, false);
             }
             
